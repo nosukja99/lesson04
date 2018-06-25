a067f405f4a96823f3bd4ae2e3bbf51508162084
nosukja99 <nosukja99@gmail.com>
2018-06-25 11:31:20 -0400

    Initial commit

---
+++ src/main/java/com/example/lesson04/Lesson04Application.java
@@ -0,0 +1,12 @@
+package com.example.lesson04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson04Application.class, args);
+	}
+}
